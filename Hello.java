96eeee1bb5fdca0c2d10bf367508deaffc661532
Ubuntu <ubuntu@ip-172-31-30-170.us-east-2.compute.internal>
2025-11-10 16:08:14 +0000

    Added second hello line to test Poll SCM auto build

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World from Java + Docker + Jenkins!");
+	System.out.println("This is an updated version - auto build test!");
     }
 }
 
